6b33c59568086a819d7dc8dfba82144971265cb4
Shalom Sims <sim16024@byui.edu>
2019-12-08 02:01:07 -0700

    Heroku bugs

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const path = require("path");
  * GET user data
  * 
 */
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.get("/login", login);
