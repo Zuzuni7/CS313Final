c6fb7ee7e3fb1e5010ea99ee488dc16f861c7a8f
Shalom Sims <sim16024@byui.edu>
2019-12-16 19:21:40 -0700

    Final Product

--- app.js
+++ app.js
@@ -4,28 +4,15 @@
  */
 var express = require("express");
 var fs = require('fs');
-const {Pool} = require('pg');
+var url = require('url');
+var {Pool} = require('pg');
 DATABASE_URL='postgres://vmbubwrksilkgw:cc626155910b11cb0687f8fd0ecdd91ea48e5d5e9e2218b90b79d9d12864b256@ec2-107-22-239-155.compute-1.amazonaws.com:5432/d6h6qif4cicgvo?ssl=true';
-const connectionString = process.env.DATABASE_URL || 'postgres://vmbubwrksilkgw:cc626155910b11cb0687f8fd0ecdd91ea48e5d5e9e2218b90b79d9d12864b256@ec2-107-22-239-155.compute-1.amazonaws.com:5432/d6h6qif4cicgvo?ssl=true';
-const pool = new Pool({connectionString: connectionString});
-//TESTING
-// var sql = "SELECT * FROM users_";
-
-// pool.query(sql, function(err, result) {
-//     // If an error occurred...
-//     if (err) {
-//         console.log("Error in query: ")
-//         console.log(err);
-//     }
-//     // Log this to the console for debugging purposes.
-//     console.log("Back from DB with result:");
-//     console.log(result.rows);
-// });     
-
-
-const app = express();
-const port = process.env.PORT || 5000;
-const path = require("path");
+var connectionString = process.env.DATABASE_URL || 'postgres://vmbubwrksilkgw:cc626155910b11cb0687f8fd0ecdd91ea48e5d5e9e2218b90b79d9d12864b256@ec2-107-22-239-155.compute-1.amazonaws.com:5432/d6h6qif4cicgvo?ssl=true';
+var pool = new Pool({connectionString: connectionString});
+var session = require('express-session');
+var app = express();
+var port = process.env.PORT || 5000;
+var path = require("path");
 //const validate = require("./login.js");
 /** 
  * GET user
@@ -35,56 +22,61 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.get("/login", login);
 app.get('/', function(req,res){
   res.sendFile('/pages/login.html', {root: __dirname + "/views"});
 });
-app.get("/logout.html", function(req,res) {
-    //end session
-    res.end;
+app.get('./pages/home.html', login);
+app.get("./pages/logout.html", function(req,res) {
+    res.sendFile('/pages/login.html', {root: __dirname + "/views"});
 });
-app.post('/views', function(req,res) {
-  const username = req.body.username;
-  const password = req.body.password;
-  console.log(username);
-  console.log(password);
-  if (req.body.username &&  req.body.password)
-  {
-      console.log(`Username: ${username} -- Password: ${password}`);
-      if (username === "admin" && password === "password") {
-        res.status(200);
-        //res.send('./home.html');
-        
-        if (typeof req.body.username === "undefined") {
-          req.body.username = username;
-        }
-        return res.sendFile('./pages/home.html',  {root: __dirname + "/views"});
-      } 
-      else 
-      {
-        res.status(401);
-        return res.sendFile('./pages/login.html',  {root: __dirname + "/views"});
-        //Wrong username or password
-      }
-    } 
-    else
-    {
-     res.status(401);
-     return res.sendFile('./pages/login.html',  {root: __dirname + "/views"});
-    //empty fields
-  }
-});
-//app.get('/views', validate.validateLogin);
+app.post('/views', login);
 
 function login(req,res) {
-    console.log("Received a request for " + req.url);
+  var username = req.body.inputUsername;
+  var password = req.body.inputPassword;
+
+  //console.log(username);
+  //console.log(password);
   
-    const data = fs.readFileSync('data.json');
-    var json = JSON.parse(data);
-    console.log("User_id: " + json.id);
-    console.log("First name: " + json.first_name);
-    console.log("Last name: " + json.last_name);
-    res.json(json);
+      var sql = "SELECT username, pw, prefered_name FROM users_ WHERE username=$1::text";
+      var params = [username];
+
+      pool.query(sql, params, function(err,db_results) {
+        if (err)
+        {
+          console.log("Getting error in pool");
+          throw err;
+          
+        }
+        else
+        {
+          var results = {success:true,list:db_results};
+          //console.log(db_results.rows[0].username);
+          //console.log(results);
+          
+          if(username == db_results.rows[0].username && password == db_results.rows[0].pw)
+          {
+            //res.status(200).json(results.rows);
+            console.log("Password matches!");
+            // if (typeof username === "undefined") {
+            //  username = username;
+            // }
+            
+
+            res.sendFile('./pages/home.html',  {root: __dirname + "/views"});
+          }
+          else
+          {
+            res.status(401);
+            console.log("username and password don't match");
+            return res.sendFile('./pages/login.html',  {root: __dirname + "/views"});
+           
+            //Wrong username or password
+          }
+          //console.log("Oops. Something went wrong and its the developers fault.");
+          //return res.sendFile('./pages/login.html',  {root: __dirname + "/views"});
+        }
+      });
 }
 
 
--- db/db.sql
+++ db/db.sql
@@ -18,6 +18,7 @@
 --    liked_video_id SERIAL PRIMARY KEY
 --    , 
 --)
+
 DROP TABLE IF EXISTS users_;
 
 
@@ -31,7 +32,11 @@ CREATE TABLE users_
 
 
 INSERT INTO users_ (username,pw,prefered_name) VALUES ('bubba', 'password', 'Yaboi-skie');
+INSERT INTO users_ (username,pw,prefered_name) VALUES ('admin', 'password', 'Admin');
+INSERT INTO users_ (username,pw,prefered_name) VALUES ('test', 'test', 'test');
 
 -- NO permissions to make an admin
 --CREATE USER FinalAdmin WITH PASSWORD 'admin';
 --GRANT SELECT, INSERT, UPDATE ON users_ TO quiet-ridge-88699;
+-- d6h6qif4cicgvo Database name
+-- USername   vmbubwrksilkgw
